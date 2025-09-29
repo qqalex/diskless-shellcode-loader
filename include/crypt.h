72d1f9966e2d933233172806fb584f251a6de11e
alex <86507185+qqalex@users.noreply.github.com>
2025-09-29 19:35:06 -0400

    Removed unused crypt

--- include/crypt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct DiffieHellman;
-struct DiffieHellman dh_init_root(int a, int p, int g);
-struct DiffieHellman dh_init(int a, int p);
-void dh_shared_secret(struct DiffieHellman *dh, int B);
-void XOR(char *data, unsigned int size, char key);
